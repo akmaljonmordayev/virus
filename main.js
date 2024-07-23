a72696ed0511a478d70f5e4a8aa2e9872246184a
akmaljonmordayev <akmaljonmordayev@gmail.com>
2024-07-23 14:39:34 +0500

    virus done

---
+++ main.js
@@ -0,0 +1,20 @@
+// let numbers = [12, 2, 45, 546, 23, 56, 12, 3453];
+
+// console.log(numbers[5]);
+
+// for (let index = 0; index < numbers.length; index++) {
+//   console.log(numbers[index]);
+// }
+
+
+// for (let index = 100; index < 1; index++) {
+//   console.log(index);
+// }
+
+
+
+
+
+for (;;) {
+  console.log("This loop will run forever!");
+}
